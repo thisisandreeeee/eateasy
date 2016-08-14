0a0b0c209801a9cf56804e962828897c070cc3aa
Andre Tan <tanandre93@gmail.com>
2016-08-14 00:34:20 -0700

    modified to output x number of businesses

--- app.py
+++ app.py
@@ -13,28 +13,38 @@ def form():
         dic['email'] = request.form.get('inputEmail')
         dic['dietary_preference'] = request.form.getlist('dietary_preference')
         dic['budget'] = request.form.get('budget')
+        dic['location'] = request.form.get('autocomplete')
 
         loc = '88 Colin P Kelly Jr St'
         businesses = yh.get_nearby_businesses(dic, loc)
-        business = choose_business(dic, businesses)
+        limit = 3
+        business = choose_business(dic, businesses, limit)
         return render_template('results.html', business = business)
     return render_template('form.html')
 
-def choose_business(dic, lst):
+def choose_business(dic, lst, limit = 3):
+    count = 0
     BUDGET_MAP = {
         '1-10': 1,
         '10-20': 2,
         '20-50': 3,
         '50+': 4
     }
+    all_items = []
     for item in lst:
         r = requests.get(item['url'])
         soup = BeautifulSoup(r.text, 'html.parser')
-        dollar_count = len(soup.find("span", { "class" : "price-range" }).text)
+        try:
+            dollar_count = len(soup.find("span", { "class" : "price-range" }).text)
+        except:
+            pass
         budget = BUDGET_MAP[dic['budget']]
         if budget <= dollar_count:
-            return item
-    return None
+            all_items.append(item)
+            count += 1
+        if count == limit:
+            return all_items
+    return all_items if all_items else None
 
 @app.route("/") #TODO: temporary, remove later
 def main():
