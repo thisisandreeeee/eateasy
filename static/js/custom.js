a8c83b4b68a13a9ed705979afda45b2de064b66d
Andre Tan <tanandre93@gmail.com>
2016-08-14 00:13:29 -0700

    add loading modal

--- static/js/custom.js
+++ static/js/custom.js
@@ -2,5 +2,12 @@
 function initMap(){
 	var input = document.getElementById('autocomplete');
     var autocomplete = new google.maps.places.Autocomplete(input);
-    
- }    
+
+}
+
+$(document).ready(function() {
+    $body = $("body");
+    $("#modal-display").click(function() {
+        $body.addClass("loading");
+    });
+});
