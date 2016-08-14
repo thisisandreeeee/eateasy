6d5e7b77c3cf208dede5f2777d45e0e2da884c87
Andre Tan <tanandre93@gmail.com>
2016-08-14 01:15:19 -0700

    increase limit to 20

--- yelp_handler.py
+++ yelp_handler.py
@@ -16,7 +16,7 @@ class YelpHandler:
     def get_nearby_businesses(self, dic, location):
         params = {
             'lang': 'en',
-            'limit': '15',
+            'limit': '20',
             'category_filter': ','.join([cat for cat in dic['dietary_preference'] if dic['dietary_preference']])
         }
         businesses = self.client.search(location, **params).businesses
