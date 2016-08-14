32e7754103158c9b77bd1a5e41cdf9827883eae2
Andre Tan <tanandre93@gmail.com>
2016-08-13 21:18:29 -0700

    add logic to query budgets from yelp

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from flask import Flask, render_template, request
 from yelp_handler import YelpHandler
+import requests
+from bs4 import BeautifulSoup
 app = Flask(__name__)
 
 yh = YelpHandler()
@@ -14,13 +16,25 @@ def form():
 
         loc = '88 Colin P Kelly Jr St'
         businesses = yh.get_nearby_businesses(dic, loc)
-        business = choose_business(businesses)
+        business = choose_business(dic, businesses)
         return render_template('results.html', business = business)
     return render_template('form.html')
 
-def choose_business(lst):
-
-    return lst[0]
+def choose_business(dic, lst):
+    BUDGET_MAP = {
+        '1-10': 1,
+        '10-20': 2,
+        '20-50': 3,
+        '50+': 4
+    }
+    for item in lst:
+        r = requests.get(item['url'])
+        soup = BeautifulSoup(r.text, 'html.parser')
+        dollar_count = len(soup.find("span", { "class" : "price-range" }).text)
+        budget = BUDGET_MAP[dic['budget']]
+        if budget <= dollar_count:
+            return item
+    return None
 
 @app.route("/") #TODO: temporary, remove later
 def main():
